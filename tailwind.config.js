4a4d4d41f28891ef9e807eb317945457e897fc90
firay24 <72513085+Firay24@users.noreply.github.com>
2023-02-16 15:17:25 +0700

    temp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
-const colors = require("tailwindcss/colors");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    // enabled: true,
-    content: ["./**/*.html", "./**/*.js"],
-  },
-  presets: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {
-      gridTemplateColumns: {        
-        // Simple 16 column grid        
-        '20': 'repeat(20, minmax(0, 1fr))',     
-      },
-      gridColumn: {
-        'span-19': 'span 19 / span 19',
-      } 
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
