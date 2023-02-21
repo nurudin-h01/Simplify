05c1d4586cb6d8f280a600036d2e25ba3e363ce6
firay24 <72513085+Firay24@users.noreply.github.com>
2023-02-21 17:27:49 +0700

    improve class page content

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -27,38 +27,38 @@ export default function Filter() {
   
   return (
     <div className="m-5 col-span-3 text-[#272837] p-6 bg-white border border-gray-200 rounded-lg shadow">
-      <p className="text-lg text-start font-bold mb-5">Filter By</p>
+      <p className="text-basea7YZ text-start font-bold mb-5">Filter By</p>
       <form onSubmit={handleSubmit(onSubmit)} className="focus:ring-green-500 focus:border-green-500">
         <div className="mb-3">
-          <label for="jeniskelamin" class="block mb-1 text-base text-gray-900">Jenis Kelamin</label>
-          <select {...register("jeniskelamin")} id="jeniskelamin"className="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
-            <option selected>Semua</option>
-            <option value="Laki-laki">Laki- laki</option>
-            <option value="Perempuan">Perempuan</option>
+          <label for="jeniskelamin" class="block mb-1 text-xs text-gray-900">Jenis Kelamin</label>
+          <select {...register("jeniskelamin")} id="jeniskelamin"className="form-select appearance-none block w-full px-3 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
+            <option className="text-xs" selected>Semua</option>
+            <option className="text-xs" value="Laki-laki">Laki- laki</option>
+            <option className="text-xs" value="Perempuan">Perempuan</option>
           </select>
         </div>
         <div className="mb-3">
-          <label for="jenis" class="block mb-1 text-base text-gray-900">Jenis</label>
-          <select {...register("jenis")} id="jenis"className="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
-            <option selected>Semua</option>
-            <option value="Laki-laki">Laki- laki</option>
-            <option value="Perempuan">Perempuan</option>
+          <label for="jenis" class="block mb-1 text-xs text-gray-900">Jenis</label>
+          <select {...register("jenis")} id="jenis"className="form-select appearance-none block w-full px-3 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
+            <option className="text-xs" selected>Semua</option>
+            <option className="text-xs" value="Laki-laki">Laki- laki</option>
+            <option className="text-xs" value="Perempuan">Perempuan</option>
           </select>
         </div>
         <div className="mb-3">
-          <label for="tempat" class="block mb-1 text-base text-gray-900">Tempat</label>
-          <select {...register("tempat")} id="tempat" className="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
-            <option selected>Semua</option>
-            <option value="Laki-laki">Laki- laki</option>
-            <option value="Perempuan">Perempuan</option>
+          <label for="tempat" class="block mb-1 text-xs text-gray-900">Tempat</label>
+          <select {...register("tempat")} id="tempat" className="form-select appearance-none block w-full px-3 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
+            <option className="text-xs" selected>Semua</option>
+            <option className="text-xs" value="Laki-laki">Laki- laki</option>
+            <option className="text-xs" value="Perempuan">Perempuan</option>
           </select>
         </div>
         <div className="mb-3">
-          <label for="tahun" class="block mb-1 text-base text-gray-900">Tahun</label>
-          <select {...register("tahun")} id="tahun"className="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
-            <option selected>Semua</option>
-            <option value="Laki-laki">Laki- laki</option>
-            <option value="Perempuan">Perempuan</option>
+          <label for="tahun" class="block mb-1 text-xs text-gray-900">Tahun</label>
+          <select {...register("tahun")} id="tahun"className="form-select appearance-none block w-full px-3 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-[#272837] focus:outline-none">
+            <option className="text-xs" selected>Semua</option>
+            <option className="text-xs" value="Laki-laki">Laki- laki</option>
+            <option className="text-xs" value="Perempuan">Perempuan</option>
           </select>
         </div>
       </form>
--- src/components/kelas.jsx
+++ src/components/kelas.jsx
@@ -16,47 +16,47 @@ export default function Kelas({namakelas}) {
         <div className="col-span-11 pl-1 w-full">
           <div className="flex w-full">
             <div className="box-1 w-full">
-              <p className="text-lg font-extrabold text-[#464646] mb-2">
+              <p className="text-lg font-bold font-poppins text-[#464646] mb-2">
                 {namakelas}
               </p>
             </div>
             <div className="box-2 ml-auto flex flex-col text-[#464646]">
-              <i class="fa-regular fa-bookmark text-[#464646] ml-auto"></i>
+              <i class="text-lg fa-regular fa-bookmark text-[#464646] ml-auto"></i>
             </div>
           </div>
-          <div className="my-2">
-              <div className="grid grid-cols-12">
+          <div className="mt-[2px]">
+              <div className="grid grid-cols-12 gap-1">
                 <div className="col-span-4">
-                  <div className="flex my-1">
-                    <i class="fa-solid text-base fa-location-dot text-[#7A7A7A]"></i>
-                    <p className="ml-2 mb-2 font-medium text-base text-[#7A7A7A]">
+                  <div className="flex my-1 w-28">
+                    <i class="fa-solid text-xs fa-location-dot text-[#7A7A7A]"></i>
+                    <p className="ml-2 mb-2 font-bold text-xs text-[#7A7A7A]">
                       Lokasi{" "}
                     </p>
                   </div>
-                  <div className="flex my-1">
-                    <i class="fa-regular fa-calendar text-base text-[#7A7A7A]"></i>
-                    <p className="ml-2 font-medium text-base text-[#7A7A7A]">
+                  <div className="flex my-1 w-28">
+                    <i class="fa-regular fa-calendar text-xs text-[#7A7A7A]"></i>
+                    <p className="ml-2 font-bold text-xs text-[#7A7A7A]">
                       Pelaksanaan
                     </p>
                   </div>
                 </div>
-                <div className="col-span-4">
-                  <div className="flex my-1">
-                    <i class="fa-solid text-base fa-location-dot text-[#7A7A7A]"></i>
-                    <p className="ml-2 mb-2 font-medium text-base text-[#7A7A7A]">
-                      Lokasi{" "}
+                <div className="col-span-4 border-l-2 px-3">
+                  <div className="flex my-1 w-28">
+                    <i class="fa-solid fa-user-group text-xs text-[#7A7A7A]"></i>
+                    <p className="ml-2 mb-2 font-bold text-xs text-[#7A7A7A]">
+                      Kouta{" "}
                     </p>
                   </div>
-                  <div className="flex my-1">
-                    <i class="fa-regular fa-calendar text-base text-[#7A7A7A]"></i>
-                    <p className="ml-2 font-medium text-base text-[#7A7A7A]">
-                      Pelaksanaan
+                  <div className="flex my-1 w-28">
+                    <i class="fa-solid fa-user-plus text-xs text-[#7A7A7A]"></i>
+                    <p className="ml-2 font-bold text-xs text-[#7A7A7A]">
+                      Pendaftar{" "}
                     </p>
                   </div>
                 </div>
                 <div className="col-span-4 flex">
                   <button className="px-4 py-2 ml-auto mt-auto bg-[#006EC5] text-white rounded font-semibold">
-                    <p>Daftar Kelas</p>
+                    <p className="text-sm">Daftar Kelas</p>
                   </button>
                 </div>
               </div>
